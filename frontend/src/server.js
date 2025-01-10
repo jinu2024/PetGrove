c72b3a2cf245d3e94589442022c4e5adae74347f
Ayush Sharma <ayushsharma3310@gmail.com>
2025-01-10 15:56:20 +0530

    server

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://petgrove.onrender.com/api/v1";
+export const server = "https://petgrove-pdfi.onrender.com/api/v1";
 
-export const backend_url = "https://petgrove.onrender.com/";
+export const backend_url = "https://petgrove-pdfi.onrender.com";
